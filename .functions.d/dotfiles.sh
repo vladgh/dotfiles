cf5689b87116a5ee7a18d589a50fba8beae5b87c
Vlad <vlad@ghn.me>
2015-01-03 12:09:31 -0600

    Fix dotfiles install

--- .functions.d/dotfiles.sh
+++ .functions.d/dotfiles.sh
@@ -5,14 +5,14 @@ link_dotfiles(){
   [[ -n $@ ]] || return 1
 
   e_header 'Symlinks'
-  bakdir="~/backups/dotfiles.$(date "+%Y_%m_%d-%H_%M_%S").bak"
+  bakdir="${HOME}/backups/dotfiles.$(date "+%Y_%m_%d-%H_%M_%S").bak"
   mkdir -p $bakdir && e_ok "Backups will be saved in '$bakdir'\n"
 
   (
   cd $HOME
   for dotfile in $@; do
     dot=$(basename $dotfile)
-    old=~/${dot}
+    old=${HOME}/${dot}
 
     if [[ -s $old ]]; then # IF old file exists
       if [[ -L $old ]]; then # Replace if old file is a symlink
@@ -36,15 +36,15 @@ link_dotfiles(){
 # Set-up permissions for dotfiles
 dotfiles_permissions(){
   e_header 'Permissions'
-  find ~/bin/ -type f -exec chmod 755 {} \; && \
+  find ${HOME}/bin/ -type f -exec chmod 755 {} \; && \
     e_ok '~/bin/* is set to 755'
-  find ~/.ssh/ -type f -name 'id_*' -exec chmod 600 {} \; && \
+  find ${HOME}/.ssh/ -type f -name 'id_*' -exec chmod 600 {} \; && \
     e_ok '~/.ssh/id* is set to 600'
 }
 
 # Clean up broken symlinks
 dotfiles_delete_broken_symlinks(){
   e_header 'Clean-up'
-  find -L ~/ -maxdepth 1 -type l -exec rm {} +
+  find -L ${HOME}/ -maxdepth 1 -type l -exec rm {} +
   e_ok 'Broken symlinks deleted'
 }
