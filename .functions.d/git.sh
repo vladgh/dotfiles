e6dbaa91b5ef1249b8876c5d5dc18c1a7cefae7b
Vlad Ghinea <vlad@ghn.me>
2015-12-09 07:17:50 -0600

    Improve git prompt

--- .functions.d/git.sh
+++ .functions.d/git.sh
@@ -1,32 +1,20 @@
 #!/usr/bin/env bash
 # Git Functions
 
-# Find if directory is a git repository
-git_is_repo() {
-  git rev-parse 2>/dev/null
-}
-
-# Prints '*' if working directory is not clean
-git_dirty() {
-  if [[ $(git status 2> /dev/null | tail -n1) != *"working directory clean"* ]];then
-    printf "*"
-  fi
-}
-
-# Prints current git branch
-git_branch() {
-  git symbolic-ref --short HEAD | tr -d '\n'
-}
-
-# Returns git prompt info
-git_prompt(){
-  if git_is_repo; then
-    printf "[" && git_branch && git_dirty && printf "]" | tr -d '\n'
-  fi
-}
-
 # Export PS1 with the git info
-if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-  PS1="\[$(tput bold)$(tput setaf 2)\]\u\[$(tput setaf 7)\]@\[$(tput setaf 4)\]\h:\[$(tput setaf 6)\]\W\[$(tput setaf 4)\]\$(git_prompt)\[$(tput setaf 6)\]$ \[$(tput sgr0)\]"
+if [ -s /etc/bash_completion.d/git-prompt ] || [ -s /usr/local/etc/bash_completion.d/git-prompt.sh ]; then
+  if [ -s /usr/local/etc/bash_completion.d/git-prompt.sh ]; then
+    # shellcheck disable=SC1091
+    . /usr/local/etc/bash_completion.d/git-prompt.sh
+  elif [ -s /etc/bash_completion.d/git-prompt ]; then
+    # shellcheck disable=SC1091
+    . /etc/bash_completion.d/git-prompt
+  fi
+  export GIT_PS1_SHOWDIRTYSTATE=true
+  export GIT_PS1_SHOWSTASHSTATE=true
+  export GIT_PS1_SHOWUNTRACKEDFILES=true
+  export GIT_PS1_SHOWCOLORHINTS=true
+  export GIT_PS1_SHOWUPSTREAM="auto"
+  export PROMPT_COMMAND='__git_ps1 "\e[0;34m\\u\e[0;36m@\h\e[0m:\W" "\\\$ "'
 fi
 
