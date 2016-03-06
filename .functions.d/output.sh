e46fa2111d09544504e036ad2581b880ecf378e2
Vlad <vlad@ghn.me>
2016-03-05 20:35:23 -0600

    Improve CLI output

--- .functions.d/output.sh
+++ .functions.d/output.sh
@@ -48,36 +48,11 @@ revers="$(tput rev)"        # reverse
 # Logging stuff.
 is_color()  { [[ $TERM =~ color ]] ;}
 is_silent() { [[ $SILENT == true ]] ;}
-e_header() {
-  is_silent || { is_color && echo -e "\n\033[1m$*\033[0m" || echo -e "\n$*" ;}
-}
-e_ok() {
-  is_silent || \
-    { is_color && echo -e "  \033[1;32m✔\033[0m  $*" || echo "  ✔  $*" ;}
-}
-e_error() {
-  is_silent || \
-    { is_color && echo -e "  \033[1;31m✖\033[0m  $*" || echo "  ✖  $*" ;}
-}
-e_warn() {
-  is_silent || \
-    { is_color && echo -e "  \033[1;33m  $*\033[0m" || echo "    $*" ;}
-}
-e_arrow() {
-  is_silent || \
-    { is_color && echo -e "  \033[0;34m➜\033[0m  $*" || echo "  ➜  $*" ;}
-}
-e_footer() {
-  is_silent || \
-    { is_color && echo -e "\n\033[1m$*\033[0m\n" || echo -e "\n$*\n" ;}
-}
-e_abort() {
-  is_silent || \
-    { is_color && echo -e "  \033[1;31m✖\033[0m  ${1}" || echo "  ✖  ${1}" ;}
-  return "${2:-1}"
-}
-e_ok() {
-  is_silent || \
-    { is_color && echo -e "  \033[1;32m✔\033[0m  $*" || echo "  ✔  $*" ;}
-}
-e_finish() { e_ok "Finished ${BASH_SOURCE[0]} at $(/bin/date "+%F %T")"; }
+e_header()  { is_silent || { is_color && printf "\n${mkbold}${bfgwht}%s${normal}" "$@" || printf "\n%s\n" "$@" ;} ;}
+e_footer()  { is_silent || { is_color && printf "\n${mkbold}${bfgwht}%s${normal}" "$@" || printf "  ➜  %s" "$@";} ;}
+e_ok()      { is_silent || { is_color && printf "  ${bfggrn}✔${normal}  %s\n" "$@" || printf "  ✔  %s" "$@" ;} ;}
+e_error()   { is_silent || { is_color && printf "  ${fgred}✖${normal}  %s\n" "$@" || printf "  ✖  %s" "$@";} ;}
+e_warn()    { is_silent || { is_color && printf "  ${fgylw}${normal}  %s\n" "$@" || printf "    %s" "$@";} ;}
+e_arrow()   { is_silent || { is_color && printf "  ${fgcyn}➜${normal}  %s\n" "$@" || printf "  ➜  %s" "$@";} ;}
+e_abort()   { e_error "$1"; return "${2:-1}" ;}
+e_finish()  { e_ok "Finished ${BASH_SOURCE[0]} at $(/bin/date "+%F %T")"; }
