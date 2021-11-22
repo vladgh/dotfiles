38452cdd260d6ad114b9aaf5f945ab02c992809f
Vlad <vlad@ghn.me>
2021-11-22 12:56:14 -0600

    Clean-up oboslete stuff

--- install.sh
+++ install.sh
@@ -9,9 +9,6 @@ IFS=$'\n\t'
 
 # VARs
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-SECRETS_S3_PATH="${SECRETS_S3_PATH:-}" # Optional S3 location for the secret files (no trailing slash)
-SECRETS_LOCAL_PATH="${SECRETS_LOCAL_PATH:-}" # Optional directory to save secret files (no trailing slash)
-TMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'tmp')
 
 # Check if command exists
 is_cmd() { command -v "$@" >/dev/null 2>&1 ;}
@@ -30,8 +27,7 @@ dotfiles_list(){
         ! -path '*/.git' \
         ! -path '*/.gitignore' \
         ! -path '*/.gitmodules' \
-        ! -path '*/.DS_Store' \
-        -o -name 'bin'
+        ! -path '*/.DS_Store'
     fi
   done
 }
@@ -59,81 +55,31 @@ dotfiles_link(){
   fi
 }
 
-# Download and decrypt secrets
-dotfiles_download_secrets(){
-  if [[ -n "$SECRETS_S3_PATH" ]] && [[ -n "$SECRETS_LOCAL_PATH" ]] && is_cmd aws; then
-    # Download the files to a temporary directory (to preserve AWS settings for the next step)
-    mkdir -p "$TMPDIR"
-    aws s3 sync "${SECRETS_S3_PATH}" "${TMPDIR}" --sse --delete --quiet --exact-timestamps || true ;
-    e_ok "Secret files downloaded from ${SECRETS_S3_PATH}"
-
-    # Back up originals
-    if [[ -d "$SECRETS_LOCAL_PATH" ]]; then
-      mv "$SECRETS_LOCAL_PATH" "${bakdir}/"
-      e_ok "Backed up original ${SECRETS_LOCAL_PATH} directory to ${bakdir}"
-    fi
-
-    # Replace originals
-    mv "$TMPDIR" "$SECRETS_LOCAL_PATH"
-    e_ok "Secret files installed at ${SECRETS_LOCAL_PATH}"
-  fi
-}
-
-# Iterate public and private dotfiles and creates links for each of them
+# Iterate dotfiles and creates links for each of them
 dotfiles_install(){
   bakdir="${HOME}/backups/dotfiles.$(date "+%Y_%m_%d-%H_%M_%S").bak"
   [[ -d "$bakdir" ]] || mkdir -p "$bakdir"
 
-  dotfiles_download_secrets
-
   (
   cd "$HOME" || exit
-  for dotfile in $(dotfiles_list "$DOTFILES" "$SECRETS_LOCAL_PATH"); do
+  for dotfile in $(dotfiles_list "$DOTFILES"); do
     dotfiles_link "$dotfile"
   done
   )
 }
 
-# Sets permissions for dotfiles
-dotfiles_permissions(){
-  if find "${HOME}/bin/" -type f -exec chmod +x {} \; 2>/dev/null; then
-    e_ok 'Made files in "bin" directory executable'
-  fi
-  if find "$SECRETS_LOCAL_PATH" -type f -exec chmod 600 {} \; 2>/dev/null; then
-    e_ok "Fixed permissions on secret files"
-  fi
-  if find "$SECRETS_LOCAL_PATH" -type d -exec chmod 700 {} \; 2>/dev/null; then
-    e_ok "Fixed permissions on secret folders"
-  fi
-}
-
 # Cleans broken symlinks in the $HOME directory
 dotfiles_delete_broken_symlinks(){
-  if find -L "${HOME}/" -maxdepth 1 -mindepth 1 -type l -exec rm {} +; then
+  if [[ -n "$(find "$HOME" -maxdepth 1 -mindepth 1 -xtype l)" ]]; then
+    find "$HOME" -maxdepth 1 -mindepth 1 -xtype l -exec rm {} +
     e_ok 'Deleted broken symlinks'
-  fi
-}
-
-# Install VIM plugins
-dotfiles_install_vim_plugins(){
-  if [[ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
-    e_ok 'Cloned Vundle'
-  fi
-  if is_cmd vim; then
-    vim +PluginInstall +qall
-    e_ok 'Installed/Updated VIM plugins'
-  else
-    e_warn 'Vim is not installed! Skipped plugins installation' >&2
   fi
 }
 
 # Script's logic
 main(){
   dotfiles_install
-  dotfiles_permissions
   dotfiles_delete_broken_symlinks
-  dotfiles_install_vim_plugins
 }
 
 main "$@"
